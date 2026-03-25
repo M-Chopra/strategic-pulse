aed9efa3330c0d69c54f20cc84a9037ec46dd1b3
M-Chopra <manavchopra704@gmail.com>
2026-03-25 19:16:35 +0530

    fix: hardcode API URL for deployment

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,19 +1,19 @@
-import axios from 'axios'
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+  baseURL: "https://strategic-pulse-1.onrender.com/api",
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
-})
+});
 
 api.interceptors.response.use(
   (response) => response,
   (error) => {
     const message =
-      error.response?.data?.message || error.message || 'An error occurred'
-    return Promise.reject(new Error(message))
+      error.response?.data?.message || error.message || "An error occurred";
+    return Promise.reject(new Error(message));
   }
-)
+);
 
-export default api
+export default api;
